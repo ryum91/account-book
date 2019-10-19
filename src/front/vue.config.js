029a3e6346c8fbff44eb75e8eb1369e2ed2189fd
Charyum Park <ryum91@gmail.com>
2019-10-20 01:19:38 +0900

    refactor: change directory tree

--- src/front/vue.config.js
+++ src/front/vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   lintOnSave: false,
   publicPath: '/',
-  outputDir: '../resources/templates',
+  outputDir: '../dist/templates',
   assetsDir: process.env.NODE_ENV === 'production' ? '../static' : '',
   productionSourceMap: false
 };
